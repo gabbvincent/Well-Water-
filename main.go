107b82b3495d2fb533b8c2b2bf5057219f6072ab
Vincent Gabb <60416073+gabbvincent@users.noreply.github.com>
2020-04-16 16:14:27 -0700

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ package main
 import "fmt"
 
 func Wellwater(a, b float64)float64{
-
+  
+//Declare variable for GallonsPerfoot as float 64
+//Declare variable for Water as float 64 
   var GallonsPerfoot float64 = a * b * .163 / 100
   var Water float64 = b * GallonsPerfoot
 
@@ -15,12 +17,20 @@ func Wellwater(a, b float64)float64{
 }
 
 func main() {
+  //Declare variable for a as float64
+  //Declare variable for b as float64
   var a float64
   var b float64
-
+  
+  //print message asking user to enter tjeir radius as inches and scan for a
   fmt.Println("Enter the radius of your well casing in inches")
   fmt.Scanln(&a)
+  
+  //Print message asking userfor their well depth in feet and scan for b
   fmt.Println("Enter how deep your well is in feet.")
   fmt.Scanln(&b)
+  
+  //Print message telling user the amount of water their well will hold by call Wellwater(a, b)
   fmt.Println("Your Well casing will hold",Wellwater(a, b),"Gallons of water.")
 }
+//end
