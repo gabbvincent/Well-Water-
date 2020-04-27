a5b3c187cadda0df7902585fd3cf3e40ceb35556
Vincent Gabb <60416073+gabbvincent@users.noreply.github.com>
2020-04-27 13:42:16 -0700

    Update main.go

--- main.go
+++ main.go
@@ -6,13 +6,17 @@ package main
 
 import "fmt"
 
-func Wellwater(a, b float64)float64{
+func Wellwater(a, b, c float64)float64{
   
 //Declare variable for GallonsPerfoot as float 64
 //Declare variable for Water as float 64 
   var GallonsPerfoot float64 = a * b * .163 / 100
   var Water float64 = b * GallonsPerfoot
-
+  if Water / c > 62.5{
+    fmt.Println("Your Well casing will hold",Water,"Gallons of water, pleanty to supply your household and no need to install another holding tank.")
+  } else {
+    fmt.Println("Your well holds", Water,"Gallons of water and that's not enough for your household, an additional tank is required to supply your household sufficiently.")
+  }
   return Water 
 }
 
@@ -21,6 +25,7 @@ func main() {
   //Declare variable for b as float64
   var a float64
   var b float64
+  var c float64
   
   //print message asking user to enter their radius as inches and scan for a
   fmt.Println("Enter the radius of your well casing in inches")
@@ -29,8 +34,10 @@ func main() {
   //Print message asking userfor their well depth in feet and scan for b
   fmt.Println("Enter how deep your well is in feet.")
   fmt.Scanln(&b)
-  
-  //Print message telling user the amount of water their well will hold by call Wellwater(a, b)
-  fmt.Println("Your Well casing will hold",Wellwater(a, b),"Gallons of water.")
+ //Ask user for the household size and scan for c
+  fmt.Println("Enter in the amount of people in your household.")
+  fmt.Scanln(&c)
+ //call Wellwater(a, b, c)
+  Wellwater(a, b, c)
 }
 //end
